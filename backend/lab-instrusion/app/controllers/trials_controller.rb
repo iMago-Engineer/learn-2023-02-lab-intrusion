50c19a8eedd9cd80f74a3186efe24a2c3f61ab0f
kuanyi-ng <raionkurane@gmail.com>
2023-02-21 18:00:54 +0900

    first working model

--- backend/lab-instrusion/app/controllers/trials_controller.rb
+++ backend/lab-instrusion/app/controllers/trials_controller.rb
@@ -15,8 +15,6 @@ class TrialsController < ApplicationController
   private
 
   def create_params
-    debugger
-
     params
     .require(:trial)
     .permit([:question_id, :count, :nickname, :solved])
