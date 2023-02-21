2882362b81689906b409170aafe6b9003381162b
kuanyi-ng <raionkurane@gmail.com>
2023-02-21 12:35:01 +0900

    add inputs

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -16,33 +16,55 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeView(),
+      home: HomeView(),
     );
   }
 }
 
 class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+  HomeView({super.key});
 
   static const keys = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  List<int> answer = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('ラボ室侵入ゲーム')),
-      body: GridView.count(
-        crossAxisCount: 3,
-        children: keys
-            .map(
-              (key) => InkWell(
-                child: Center(child: Text('$key')),
-                onTap: () {
-                  if (kDebugMode) {
-                    print(key);
-                  }
-                },
-              ),
-            )
-            .toList(),
+      body: Column(
+        children: [
+          ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: MediaQuery.of(context).size.height * 0.6,
+            ),
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: keys
+                  .map(
+                    (key) => InkWell(
+                      child: Center(child: Text('$key')),
+                      onTap: () {
+                        if (kDebugMode) {
+                          print(key);
+                        }
+                        answer.add(key);
+                      },
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 32),
+            child: Text('$answer'),
+          ),
+          TextButton(
+              onPressed: () {
+                print(answer);
+              },
+              child: const Text('答え合わせ'))
+        ],
       ),
     );
   }
